72c1d1aeb47044bda4cd46771c7472bb9755ce70
Henrik Hasell <henrik@prosys.com>
2018-06-08 12:51:36 +0100

    Added nicely styled navbar.

--- themes/henrik/functions.php
+++ themes/henrik/functions.php
@@ -1,6 +1,18 @@
 <?php
 require_once get_template_directory() . '/plugins/class-wp-bootstrap-navwalker.php';
 
+function bootstrap_nav()
+{
+	wp_nav_menu( array(
+            'theme_location'    => 'header-menu',
+            'depth'             => 2,
+            'container'         => 'false',
+            'menu_class'        => 'nav navbar-nav',
+            'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+            'walker'            => new wp_bootstrap_navwalker())
+    );
+}
+
 class Empty_Menu_Walker extends Walker_Nav_Menu {
 	function start_lvl(&$output, $depth = 0, $args = array()){
 	}
@@ -13,6 +25,7 @@ class Empty_Menu_Walker extends Walker_Nav_Menu {
 	function display_element( $element, &$children_elements, $max_depth, $depth, $args, &$output ) {
 	}
 }
+
 class Minimal_Menu_Walker extends Walker_Nav_Menu {
 	function start_lvl(&$output, $depth = 0, $args = array()){
 		$output .= "<ol>";
